606fa0ca77c0e2e53ed9a70fde37560b8c118576
kit486759 <kit486759@gmail.com>
2021-07-19 15:21:39 -0700

    -a

--- src/components/carousel/Item.js
+++ src/components/carousel/Item.js
@@ -9,19 +9,8 @@ const Item = (props) => {
   
   return (
     <div className="item">
-      <Link to={"/ciccc-react-midterm-team-project/detail/" + props.item.id} style={{textDecoration:'none'}}>
-        <img
-          src={props.item.image}
-          onMouseEnter={() => setHover(true)}
-          onMouseLeave={() => setHover(false)}
-        />
-        {itemDescription === true && 
-         <div className="description"><p>{props.item.title}</p></div>
-        } 
-       
-      </Link>
-      
-      {hover !== false && (
+
+{hover !== false && (
         <div style={{ pointerEvents: "none" }} className="modal">
           <div className="modalInfo">
             <ul>
@@ -44,6 +33,41 @@ const Item = (props) => {
           </div>
         </div>
       )}
+      <Link to={"/ciccc-react-midterm-team-project/detail/" + props.item.id} style={{textDecoration:'none'}}>
+        <img
+          src={props.item.image}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        />
+        {itemDescription === true && 
+         <div className="description"><p>{props.item.title}</p></div>
+        } 
+       
+      </Link>
+      
+      {/* {hover !== false && (
+        <div style={{ pointerEvents: "none" }} className="modal">
+          <div className="modalInfo">
+            <ul>
+              <Link
+                style={{ pointerEvents: "auto" }}
+                to={"/ciccc-react-midterm-team-project/category/" + props.item.category}
+                style={{
+                  pointerEvents: "none",
+                  textDecoration: "none",
+                  margin: 0,
+                }}
+              >
+                <li>From "{props.item.category}"</li>
+              </Link>
+              <li>{props.item.title}</li>
+              <li>
+                Price: ${(Math.round(props.item.price * 10) / 10).toFixed(2)}
+              </li>
+            </ul>
+          </div>
+        </div>
+      )} */}
     </div>
   );
 };
